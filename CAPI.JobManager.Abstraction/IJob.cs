812098276b0b6a0ee9947913dd4096c46ef13f4f
Mehdi Nadji Tehrani <m.nadji.t@gmail.com>
2017-12-15 10:58:05 +1100

    Before removing CAPI.Domain and restructuring the solution

--- CAPI.JobManager.Abstraction/IJob.cs
+++ CAPI.JobManager.Abstraction/IJob.cs
@@ -7,11 +7,7 @@ namespace CAPI.JobManager.Abstraction
     public interface IJob<T>
     {
         IDicomStudy DicomStudyFixed { get; set; }
-        IDicomSeries DicomSeriesFixed { get; set; }
-        string DicomSeriesFixedFolderPath { get; set; }
         IDicomStudy DicomStudyFloating { get; set; }
-        IDicomSeries DicomSeriesFloating { get; set; }
-        string DicomSeriesFloatingFolderPath { get; set; }
         IList<IIntegratedProcess> IntegratedProcesses { get; set; }
         IList<IDestination> Destinations { get; set; }
 
