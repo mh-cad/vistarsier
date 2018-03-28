8fd8d4d2b65b454823a7c32ca13059c03046bc29
Mehdi Nadji Tehrani <m.nadji.t@gmail.com>
2018-03-28 16:00:47 +1100

    Add Database and FileSystem test to new CAPI.Tests project

---
+++ CAPI.Tests/Database/Database.cs
@@ -0,0 +1,46 @@
+﻿using CAPI.Agent_Console;
+using CAPI.Agent_Console.Abstractions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Unity;
+using Unity.Lifetime;
+
+namespace CAPI.Tests.Database
+{
+    [TestClass]
+    public class DatabaseIntegratedTests
+    {
+        private IAgentConsoleRepository _agentConsoleRepository;
+
+        [TestInitialize]
+        public void TestInit()
+        {
+            var container = CreateContainerCore();
+            _agentConsoleRepository = container.Resolve<IAgentConsoleRepository>();
+        }
+
+        [TestMethod]
+        public void CheckDbConnection()
+        {
+            if (!_agentConsoleRepository.DbIsAvailable()) Assert.Fail("No access to CAPI database.");
+        }
+
+        [TestMethod]
+        public void CheckDbVerifiedMriTable()
+        {
+            if (!_agentConsoleRepository.DbTableVerifiedMriExists()) Assert.Fail("Table [VerifiedMri] is missing in CAPI database.");
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+
+        }
+
+        private static IUnityContainer CreateContainerCore()
+        {
+            var container = new UnityContainer();
+            container.RegisterType<IAgentConsoleRepository, AgentConsoleRepository>(new TransientLifetimeManager());
+            return container;
+        }
+    }
+}
---
+++ CAPI.Tests/FileSystem/FileSystem.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+
+namespace CAPI.Tests.FileSystem
+{
+    [TestClass]
+    public class FileSystem
+    {
+        private string _workingDir;
+        private string _sourcePath;
+        private string _targetPath;
+
+        [TestInitialize]
+        public void TestInit()
+        {
+            _workingDir = Environment.CurrentDirectory;
+            _sourcePath = $@"{_workingDir}\source-test";
+            _targetPath = $@"{_workingDir}\target-test";
+
+            if (Directory.Exists(_sourcePath)) Directory.Delete(_sourcePath, true);
+            if (Directory.Exists(_targetPath)) Directory.Delete(_targetPath, true);
+        }
+
+        [TestMethod]
+        public void CopyFolderRecursively()
+        {
+            // Arrange
+            Directory.CreateDirectory(_sourcePath);
+            Directory.CreateDirectory($@"{_sourcePath}\1");
+            Directory.CreateDirectory($@"{_sourcePath}\1\1-1");
+            var filePath = $@"{_sourcePath}\1\1-1\test.txt";
+            const string fileContent = "This is a test";
+            File.AppendAllText(filePath, fileContent);
+
+            // Act
+            Common.Services.FileSystem.CopyDirectory(_sourcePath, _targetPath);
+
+            // Assert
+            Assert.IsTrue(File.Exists(filePath));
+            var contentFromFile = File.ReadAllText(filePath);
+            Assert.IsTrue(contentFromFile == fileContent);
+        }
+
+        [TestCleanup]
+        public void TestCleanUp()
+        {
+            if (Directory.Exists(_sourcePath)) Directory.Delete(_sourcePath, true);
+            if (Directory.Exists(_targetPath)) Directory.Delete(_targetPath, true);
+        }
+    }
+}
