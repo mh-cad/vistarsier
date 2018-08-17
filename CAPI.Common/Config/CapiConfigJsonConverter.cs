203e95537a258e5e9e2dacd1c5475dbd5f38dc9e
Mehdi Nadji Tehrani <m.nadji.t@gmail.com>
2018-08-17 15:46:52 +1000

    Fix bugs and add JsonConverter for CapiConfig

---
+++ CAPI.Common/Config/CapiConfigJsonConverter.cs
@@ -0,0 +1,58 @@
+﻿using CAPI.Dicom.Abstraction;
+using CAPI.Dicom.Model;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+
+namespace CAPI.Common.Config
+{
+
+    public class CapiConfigJsonConverter : JsonConverter
+    {
+        public override bool CanConvert(Type type)
+        {
+            return true;
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            var jsonToken = JToken.Load(reader);
+
+            var capiConfig = new CapiConfig
+            {
+                DicomConfig = new DicomConfig
+                {
+                    LocalNode = new DicomNode(),
+                    RemoteNodes = new List<IDicomNode>()
+                },
+
+                DefaultRecipePath = jsonToken["DefaultRecipePath"].ToString(),
+                ManualProcessPath = jsonToken["ManualProcessPath"].ToString(),
+                AgentDbConnectionString = jsonToken["AgentDbConnectionString"].ToString(),
+                Hl7ProcessPath = jsonToken["Hl7ProcessPath"].ToString(),
+                RunInterval = jsonToken["RunInterval"].ToString(),
+                ImgProcConfig = JsonConvert.DeserializeObject<ImgProcConfig>(jsonToken["ImgProcConfig"].ToString()),
+                TestsConfig = JsonConvert.DeserializeObject<TestsConfig>(jsonToken["TestsConfig"].ToString())
+            };
+
+            // Deserialize DicomConfig
+            dynamic dicomConfig = jsonToken["DicomConfig"];
+
+            capiConfig.DicomConfig.DicomServicesExecutablesPath = dicomConfig.DicomServicesExecutablesPath.Value;
+
+            capiConfig.DicomConfig.LocalNode = JsonConvert.DeserializeObject<DicomNode>(dicomConfig.LocalNode.ToString());
+
+            var remoteNodes = JsonConvert.DeserializeObject<List<dynamic>>(dicomConfig.RemoteNodes.ToString());
+            foreach (var remoteNode in remoteNodes)
+                capiConfig.DicomConfig.RemoteNodes.Add(JsonConvert.DeserializeObject<DicomNode>(remoteNode.ToString()));
+
+            return capiConfig;
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            serializer.Serialize(writer, value);
+        }
+    }
+}
