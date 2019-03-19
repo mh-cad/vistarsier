dfba8a2f890b9168ce81854fd0a4d83fb74ab3a3
plwp <patrick.prendergast@unimelb.edu.au>
2019-03-19 17:17:34 +1100

    Added a quick and dirty Nifti viewer to allow some kind of front end.

---
+++ NiftiView/Form1.cs
@@ -0,0 +1,120 @@
+﻿using CAPI.Common.Config;
+using CAPI.ImageProcessing.Abstraction;
+using Unity;
+using Unity.log4net;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MathNet.Numerics.Statistics;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace NiftiView
+{
+    public partial class Form1 : Form
+    {
+        //////////////////////
+        private NiftiController _niftiControllerA;
+        private NiftiController _niftiControllerB;
+        private HistogramController _histogramController;
+        private int _currentSlice;
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+            // Setup chart
+            chart1.ChartAreas[0].AxisX.MajorGrid.LineWidth = 0;
+            chart1.ChartAreas[0].AxisY.MajorGrid.LineWidth = 0;
+            chart1.Series.Remove(chart1.Series.First());
+
+            _niftiControllerA = new NiftiController(pictureBoxA, lblcurrentPoint);
+            _niftiControllerB = new NiftiController(pictureBoxB, lblcurrentPoint);
+
+            _histogramController = new HistogramController(chart1, _niftiControllerA, _niftiControllerB, sor, eor);
+            _niftiControllerA.HistogramController = _histogramController;
+            _niftiControllerB.HistogramController = _histogramController;
+
+            this.KeyDown += KeyListener;
+        }
+
+        // EVENTS
+
+        private void KeyListener(object sender, EventArgs e)
+        {
+            //var mouseE = (MouseEventArgs)e;
+
+            //lblcurrentPoint.Text = $@"Mouse scroll = {mouseE.Delta}";
+            var keyE = (KeyEventArgs)e;
+
+            if (keyE.KeyCode == Keys.Q) sor.UpButton();
+            else if (keyE.KeyCode == Keys.A) sor.DownButton();
+            else if (keyE.KeyCode == Keys.E) eor.UpButton();
+            else if (keyE.KeyCode == Keys.D) eor.DownButton();
+            try
+            {
+
+                if (keyE.KeyCode == Keys.W)
+                {
+                    _currentSlice = Math.Max(0, _currentSlice - 1);
+                }
+        
+                else if (keyE.KeyCode == Keys.S)
+                {
+                    _niftiControllerA.Nifti.GetDimensions(_niftiControllerA.SliceType, out int widthA, out int heightA, out int nSlicesA);
+                    _niftiControllerB.Nifti.GetDimensions(_niftiControllerA.SliceType, out int widthB, out int heightB, out int nSlicesB);
+                    var maxSlices = Math.Min(nSlicesA - 1, nSlicesB - 1);
+                    _currentSlice = Math.Min(_currentSlice + 1, maxSlices);
+                }
+
+                _niftiControllerA.CurrentSlice = _currentSlice;
+                _niftiControllerB.CurrentSlice = _currentSlice;
+
+            } catch (Exception ex)
+            {
+
+            }
+        }
+
+        private void axialToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            _niftiControllerA.SliceType = SliceType.Axial;
+            _niftiControllerB.SliceType = SliceType.Axial;
+        }
+
+        private void coronalToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            _niftiControllerA.SliceType = SliceType.Coronal;
+            _niftiControllerB.SliceType = SliceType.Coronal;
+        }
+
+        private void sagittalToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            _niftiControllerA.SliceType = SliceType.Sagittal;
+            _niftiControllerB.SliceType = SliceType.Sagittal;
+        }
+
+        private void mnuFileOpenA_Click(object sender, EventArgs e)
+        {
+            var fileDialog = new OpenFileDialog();
+            if (fileDialog.ShowDialog() == DialogResult.OK)
+            {
+                _niftiControllerA.LoadNiftiFile(fileDialog.FileName);
+            }
+        }
+
+        private void mnuFileOpenB_Click(object sender, EventArgs e)
+        {
+            var fileDialog = new OpenFileDialog();
+            if (fileDialog.ShowDialog() == DialogResult.OK)
+            {
+                _niftiControllerB.LoadNiftiFile(fileDialog.FileName);
+            }
+        }
+    }
+}
---
+++ NiftiView/HistogramController.cs
@@ -0,0 +1,138 @@
+﻿using MathNet.Numerics.Statistics;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace NiftiView
+{
+    class HistogramController
+    {
+        private Chart _chart;
+        private NiftiController _niftiControllerA;
+        private NiftiController _niftiControllerB;
+        private Histogram _histA;
+        private Histogram _histB;
+        private NumericUpDown _startOfRange;
+        private NumericUpDown _endOfRange;
+        private DataPoint _highlighted;
+        private Color _oldColor;
+
+        private const int HIST_BINS = 256;
+
+        public HistogramController(
+            Chart chart, NiftiController niftiControllerA, NiftiController niftiControllerB,
+            NumericUpDown startOfRange, NumericUpDown endOfRange)
+        {
+            _chart = chart;
+            _niftiControllerA = niftiControllerA;
+            _niftiControllerB = niftiControllerB;
+            _startOfRange = startOfRange;
+            _endOfRange = endOfRange;
+
+            _startOfRange.ValueChanged += RangeChanged;
+            _endOfRange.ValueChanged += RangeChanged;
+        }
+
+        public void UpdateData()
+        {
+            int dataAlen = _niftiControllerA != null && _niftiControllerA.Nifti != null ? _niftiControllerA.Nifti.voxels.Length : 0;
+            int dataBlen = _niftiControllerB != null && _niftiControllerB.Nifti != null ? _niftiControllerB.Nifti.voxels.Length : 0;
+
+            double[] dataA = new double[dataAlen];
+            double[] dataB = new double[dataBlen];
+
+            if(dataAlen > 0)Array.Copy(_niftiControllerA.Nifti.voxels, dataA, dataA.Length);
+            if(dataBlen > 0)Array.Copy(_niftiControllerB.Nifti.voxels, dataB, dataB.Length);
+
+            double upper = 0;
+            double lower = 0;
+
+            if (dataAlen <= 0 || dataBlen <= 0)
+            {
+                if (dataAlen > 0)
+                {
+                    lower = dataA.Min();
+                    upper = dataA.Max();
+                }
+                else if (dataBlen > 0)
+                {
+                    lower = dataB.Min();
+                    upper = dataB.Max();
+                }
+            }
+            else
+            {
+                lower = Math.Min(dataA.Min(), dataB.Min());
+                upper = Math.Max(dataA.Max(), dataB.Max());
+            }
+
+
+            try { _histA = new Histogram(dataA, HIST_BINS, lower, upper); } catch (Exception e) { }
+            try { _histB = new Histogram(dataB, HIST_BINS, lower, upper); } catch (Exception e) { }
+
+            // Set range box values.
+            _startOfRange.Minimum = 0;
+            _startOfRange.Maximum = HIST_BINS;
+            _startOfRange.Value = _startOfRange.Minimum;
+            _endOfRange.Minimum = _startOfRange.Minimum;
+            _endOfRange.Maximum = _startOfRange.Maximum;
+            _endOfRange.Value = _endOfRange.Maximum;
+
+            UpdateChart();
+        }
+
+        public void HighlightValue(NiftiController niftiController, double value)
+        {
+            int seriesIdx = 0;
+            if (niftiController == _niftiControllerA) seriesIdx = 0;
+            if (niftiController == _niftiControllerB) seriesIdx = 1;
+
+            Series series = _chart.Series[seriesIdx]; 
+            if (_highlighted != null && _oldColor != null) _highlighted.Color = _oldColor;
+            DataPointCollection points = series.Points;
+
+            var hist = seriesIdx == 0 ? _histA : _histB; //The bucket should be synced anyway.
+
+            var index = hist.GetBucketIndexOf(value);
+            if (index >= _startOfRange.Value && index < _endOfRange.Value)
+            {
+                _highlighted = points[index - (int)(_startOfRange.Value)];
+                _oldColor = series.Color;
+                _highlighted.Color = Color.Red;
+            }
+        }
+
+        private void RangeChanged(object sender, EventArgs e)
+        {
+            UpdateChart();
+        }
+
+        private void UpdateChart()
+        {
+            _chart.Series.Clear();
+            AddHistToSeries(_histA, _chart.Series.Add("A Distriution"));
+            AddHistToSeries(_histB, _chart.Series.Add("B Distriution"));
+        }
+
+        private void AddHistToSeries(Histogram hist, Series series)
+        {
+            // Remove space between bars
+            series["PointWidth"] = "1";
+            // Remove legend label
+            series.IsVisibleInLegend = false;
+
+            if (hist == null) return;
+
+            for (int i = (int)_startOfRange.Value; i <= (int)_endOfRange.Value && i < hist.BucketCount; ++i)
+            {
+                series.Points.Add(hist[i].Count);
+                series.Points.Last().AxisLabel = hist[i].LowerBound.ToString();
+            }
+        }
+    }
+}
