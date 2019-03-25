ee889cd569c844a8b248ec8ed9e6733e24aa637c
Prendergast <Patrick.Prendergast@mh.org.au>
2019-03-26 06:45:40 +1100

    Image processing updates

---
+++ CAPI.ImageProcessing/BiasCorrection.cs
@@ -0,0 +1,56 @@
+﻿using CAPI.ImageProcessing.Abstraction;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAPI.ImageProcessing
+{
+    public class BiasCorrection
+    { 
+        //public static INifti BrainSuiteBFC(INifti input, string args, DataReceivedEventHandler updates = null)
+        //{
+
+        //    // Setup our temp file names.
+        //    string niftiInPath = TEMPDIR + input.GetHashCode() + ".bsbfc.in.nii";
+        //    string niftiOutPath = TEMPDIR + input.GetHashCode() + ".bsbfc.out.nii";
+        //    // Write nifti to temp directory.
+        //    input.WriteNifti(niftiInPath);
+
+        //    args += $"-i {niftiInPath} -o {niftiOutPath}";
+
+        //    Tools.ExecProcess("../../../ThirdPartyTools/bfc.exe", args, updates);
+
+        //    INifti output = new Nifti();
+        //    output.ReadNifti(niftiOutPath);
+
+        //    return output;
+        //}
+
+        //public static INifti BrainSuiteBFC(INifti input, INifti mask, string args, DataReceivedEventHandler updates = null)
+        //{
+        //    return null;
+        //}
+
+        public static INifti AntsN4(INifti input, DataReceivedEventHandler updates = null)
+        {
+            // Setup our temp file names.
+            string niftiInPath = Tools.TEMPDIR + input.GetHashCode() + ".antsN4.in.nii";
+            string niftiOutPath = Tools.TEMPDIR + input.GetHashCode() + ".antsN4.out.nii";
+            // Write nifti to temp directory.
+            input.WriteNifti(niftiInPath);
+
+            var args = $"-i {niftiInPath} -o {niftiOutPath}";
+
+            Tools.ExecProcess("../../../ThirdPartyTools/ants/N4BiasFieldCorrection.exe", args, updates);
+
+            INifti output = input.DeepCopy();
+            output.ReadNifti(niftiOutPath);
+
+            return output;
+        }
+    }
+}
