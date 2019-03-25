be33bd80c69af25099b74a77fc1139ca1d6f43a8
Prendergast <Patrick.Prendergast@mh.org.au>
2019-03-26 06:44:45 +1100

    Changes to image processing chain (ongiong)

--- CAPI.ImageProcessing/NiftiHeader.cs
+++ CAPI.ImageProcessing/NiftiHeader.cs
@@ -43,5 +43,54 @@ namespace CAPI.ImageProcessing
         public float[] srow_z { get; set; }
         public string intent_name { get; set; }
         public string magic { get; set; }
+
+        public INiftiHeader DeepCopy()
+        {
+            NiftiHeader copy = new NiftiHeader();
+
+            copy.sizeof_hdr = sizeof_hdr;
+            copy.dim_info = dim_info;
+            copy.dim = new short[dim.Length];
+            dim.CopyTo(copy.dim, 0);
+            copy.intent_p1 = intent_p1;
+            copy.intent_p2 = intent_p2;
+            copy.intent_p3 = intent_p3;
+            copy.intent_code = intent_code;
+            copy.datatype = datatype;
+            copy.bitpix = bitpix;
+            copy.slice_start = slice_start;
+            copy.pix_dim = new float[pix_dim.Length];
+            pix_dim.CopyTo(copy.pix_dim, 0);
+            copy.vox_offset = vox_offset;
+            copy.scl_slope = scl_slope;
+            copy.scl_inter = scl_inter;
+            copy.slice_end = slice_end;
+            copy.slice_code = slice_code;
+            copy.xyzt_units = xyzt_units;
+            copy.cal_max = cal_max;
+            copy.cal_min = cal_min;
+            copy.slice_duration = slice_duration;
+            copy.toffset = toffset;
+            copy.descrip = descrip;
+            copy.aux_file = aux_file;
+            copy.qform_code = qform_code;
+            copy.sform_code = sform_code;
+            copy.quatern_b = quatern_b;
+            copy.quatern_c = quatern_c;
+            copy.quatern_d = quatern_d;
+            copy.qoffset_x = qoffset_x;
+            copy.qoffset_y = qoffset_y;
+            copy.qoffset_z = qoffset_z;
+            copy.srow_x = new float[srow_x.Length];
+            srow_x.CopyTo(copy.srow_x, 0);
+            copy.srow_y = new float[srow_y.Length];
+            srow_y.CopyTo(copy.srow_y, 0);
+            copy.srow_z = new float[srow_z.Length];
+            srow_z.CopyTo(copy.srow_z, 0);
+            copy.intent_name = intent_name;
+            copy.magic = magic;
+
+            return copy;
+        }
     }
 }
---
+++ CAPI.ImageProcessing/Tools.cs
@@ -0,0 +1,38 @@
+﻿using CAPI.ImageProcessing.Abstraction;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAPI.ImageProcessing
+{
+    public class Tools
+    {
+        public const string TEMPDIR = "./temp";
+
+        public static void ExecProcess(string filename, string args, DataReceivedEventHandler updates = null)
+        { 
+            var process = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = filename,
+                    Arguments = args,
+                    CreateNoWindow = true,
+                    UseShellExecute = false,
+                    RedirectStandardError = true,
+                    RedirectStandardOutput = true,
+                    RedirectStandardInput = true,
+                }
+            };
+
+            process.Start();
+            process.BeginOutputReadLine();
+            process.OutputDataReceived += updates;
+
+            process.WaitForExit();
+        }
+    }
+}
