20bd2f0edd4c08da13fa4ff707c6335d10a6583c
Prendergast <Patrick.Prendergast@mh.org.au>
2019-03-26 06:45:40 +1100

    Image processing updates

---
+++ CAPI.ImageProcessing/BrainExtraction.cs
@@ -0,0 +1,31 @@
+﻿using CAPI.ImageProcessing.Abstraction;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAPI.ImageProcessing
+{
+    public class BrainExtraction
+    {
+        public static INifti BrainSuiteBSE(INifti input, DataReceivedEventHandler updates = null)
+        {
+            // Setup our temp file names.
+            string niftiInPath = Tools.TEMPDIR + input.GetHashCode() + ".bse.in.nii";
+            string niftiOutPath = Tools.TEMPDIR + input.GetHashCode() + ".bse.out.nii";
+            // Write nifti to temp directory.
+            input.WriteNifti(niftiInPath);
+
+            var args = $"-i {niftiInPath} -o {niftiOutPath}";
+
+            Tools.ExecProcess("../../../ThirdPartyTools/brain_suite/bse.exe", args, updates);
+
+           // INifti output = input.DeepCopy(); // Sometimes this messes with the header and gives us a 4-up???
+            input.ReadNifti(niftiOutPath);
+
+            return input;
+        }
+    }
+}
