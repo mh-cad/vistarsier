9d5cca82b535b473e634b197632fbecc4c976371
plwp <patrick.prendergast@unimelb.edu.au>
2019-04-04 10:19:06 +1100

    Direct file in / out versions of n4, BSE, registration.

--- CAPI.ImageProcessing/Registration.cs
+++ CAPI.ImageProcessing/Registration.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,6 +18,8 @@ namespace CAPI.ImageProcessing
             string niftiRefPath = Tools.TEMPDIR + floating.GetHashCode() + ".cmtkrego.ref.nii";
             string niftiOutPath = Tools.TEMPDIR + floating.GetHashCode() + ".cmtkrego.out.nii";
             string regOutPath = Tools.TEMPDIR + floating.GetHashCode() + ".cmtkrego.reg";
+
+           // Directory.CreateDirectory(regOutPath);
 
             // Write nifti to temp directory.
             floating.WriteNifti(niftiInPath);
@@ -34,6 +37,24 @@ namespace CAPI.ImageProcessing
             floating.ReadNifti(niftiOutPath);
 
             return floating;
+        }
+
+        public static string CMTKRegistration(string floatingFile, string referenceFile, DataReceivedEventHandler updates = null)
+        {
+            string niftiInPath = floatingFile;
+            string niftiRefPath = referenceFile;
+            string niftiOutPath = floatingFile + ".cmtkrego.out.nii";
+            string regOutPath = floatingFile + ".cmtkrego.reg";
+
+            Environment.SetEnvironmentVariable("CMTK_WRITE_UNCOMPRESSED", "1");
+
+            var args = $"-o {regOutPath} {niftiRefPath} CAPI.Cmd.pdb";
+            Tools.ExecProcess("../../../ThirdPartyTools/CMTK/registration.exe", args, updates);
+
+            args = $"-o {niftiOutPath} --floating {niftiInPath} {niftiRefPath} {regOutPath}";
+            Tools.ExecProcess("../../../ThirdPartyTools/CMTK/reformatx.exe", args, updates);
+
+            return niftiOutPath;
         }
     }
 }
