bcf1af1b1da8028e17c9c0e4ded4849606eff6ff
Prendergast <Patrick.Prendergast@mh.org.au>
2019-04-05 12:05:30 +1100

    Comments for ImageProcessing functions. Compare seems to be working okay, needs more testing.

--- CAPI.ImageProcessing/Tools.cs
+++ CAPI.ImageProcessing/Tools.cs
@@ -3,10 +3,19 @@ using System.Diagnostics;
 
 namespace CAPI.ImageProcessing
 {
+    /// <summary>
+    /// Helper tools for the Image processing library.
+    /// </summary>
     public class Tools
     {
         public const string TEMPDIR = "temp";
 
+        /// <summary>
+        /// Execyte a processs
+        /// </summary>
+        /// <param name="filename">The file to be executed</param>
+        /// <param name="args">Arguments to be passed on execution</param>
+        /// <param name="updates">Event handler to handle updates.</param>
         public static void ExecProcess(string filename, string args, DataReceivedEventHandler updates = null)
         { 
             var process = new Process
@@ -32,6 +41,12 @@ namespace CAPI.ImageProcessing
             process.WaitForExit();
         }
 
+        /// <summary>
+        /// Converts the given Dicom path to a nifti file which is loaded and returned as a nifti object.
+        /// </summary>
+        /// <param name="dicomPath">Path to the DICOM</param>
+        /// <param name="updates">Event handler to handle updates.</param>
+        /// <returns></returns>
         public static INifti Dcm2Nii(string dicomPath, DataReceivedEventHandler updates = null)
         {
             var outFile = TEMPDIR + dicomPath.GetHashCode() + ".dcm2nii";
