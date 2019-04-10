92b8980af428f93d4e50e43ddee9b9504c75487b
Prendergast <Patrick.Prendergast@mh.org.au>
2019-04-10 16:04:20 +1000

    Switch over to new and improved (and more like the old one) Image processing chain.

--- CAPI.ImageProcessing.Abstraction/IImageProcessor.cs
+++ CAPI.ImageProcessing.Abstraction/IImageProcessor.cs
@@ -2,15 +2,6 @@
 {
     public interface IImageProcessor
     {
-        void ExtractBrainMask(string inNii, string bseParams, string outBrainNii, string outMaskNii);
-        string Registration(string refNii, string priorNii, string outPriorReslicedNii, string seriesType);
-        void BiasFieldCorrection(string inNii, string mask, string bfcParams, string outNii);
-        void Normalize(string niftiFilePath, string maskFilePath, SliceType sliceType, string lookupTable);
-        void Normalize(string niftiFilePath, string maskFilePath, SliceType sliceType, int mean, int std, int widthRange);
-        void Compare(
-            string currentNiiFile, string priorNiiFile, string lookupTableFile, SliceType sliceType,
-            string resultNiiFile, string currentResliced = "", string currentReslicedMask = "");
-
         void ExtractBrainRegisterAndCompare(
             string currentNii, string priorNii, string referenceNii, string[] lookupTablePaths, SliceType sliceType,
             bool extractBrain, bool register, bool biasFieldCorrect,
@@ -21,12 +12,5 @@
             string[] lookupTablePaths, SliceType sliceType,
             bool extractBrain, bool register, bool biasFieldCorrect,
             string[] resultNiis, string outPriorReslicedNii);
-
-        // TODO1: Remove when done experimenting
-        #region Experimental
-        void CompareUsingNictaCode(string fixedFile, string floatingFile, string fixedMaskFile,
-                                   string nictaPosResultFilePath, string nictaNegResultFilePath,
-                                   string colormapConfigFilePath, bool ignoreErrors);
-        #endregion
     }
 }
