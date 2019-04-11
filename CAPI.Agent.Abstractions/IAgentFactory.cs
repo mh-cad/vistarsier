c623f72e7361bc409c7248855a7fbee45e22fbbd
Prendergast <Patrick.Prendergast@mh.org.au>
2019-04-11 16:26:54 +1000

    Exciting new comments!

--- CAPI.Agent.Abstractions/IAgentFactory.cs
+++ CAPI.Agent.Abstractions/IAgentFactory.cs
@@ -8,10 +8,31 @@ namespace CAPI.Agent.Abstractions
 {
     public interface IAgentFactory
     {
+        /// <summary>
+        /// Creates a new IAgent.
+        /// </summary>
+        /// <param name="args"></param>
+        /// <param name="dicomFactory"></param>
+        /// <param name="imgProcFactory"></param>
+        /// <param name="fileSystem"></param>
+        /// <param name="processBuilder"></param>
+        /// <param name="log"></param>
+        /// <returns></returns>
         IAgent CreateAgent(string[] args, IDicomFactory dicomFactory,
             IImageProcessingFactory imgProcFactory,
             IFileSystem fileSystem, IProcessBuilder processBuilder, ILog log);
 
+        /// <summary>
+        /// Creates a new Agent.Abstractions.IImageProcessor
+        /// </summary>
+        /// <param name="dicomServices"></param>
+        /// <param name="imgProcFactory"></param>
+        /// <param name="fileSystem"></param>
+        /// <param name="processBuilder"></param>
+        /// <param name="imfProcConfig"></param>
+        /// <param name="log"></param>
+        /// <param name="context"></param>
+        /// <returns></returns>
         IImageProcessor CreateAgentImageProcessor(
             IDicomServices dicomServices, IImageProcessingFactory imgProcFactory,
             IFileSystem fileSystem, IProcessBuilder processBuilder, IImgProcConfig imfProcConfig, ILog log, IAgentRepository context);
