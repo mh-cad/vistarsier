8d42c6089a8f1704588abdb5c25a28e09147c0c5
plwp <Patrick.Prendergast@mh.org.au>
2019-04-30 12:06:48 +1000

    Added antsRegistration

--- CAPI.Nifti/Processing/Registration.cs
+++ CAPI.Nifti/Processing/Registration.cs
@@ -79,5 +79,45 @@ namespace CAPI.NiftiLib.Processing
             return niftiOutPath;
         }
 
+        public static string ANTSRegistration(string floatingFile, string fixedFile, DataReceivedEventHandler updates = null)
+        {
+            string niftiOutPath = floatingFile + "warped.nii";
+
+            // ANTS didn't like me splitting args into a nicely tabbed string so it's all one big line.
+            // --dimensionality 3 :: we have a 3-d image
+            // --float 1 :: we want to keep things floating point
+            // --interpolation Linear :: using linear interpolation for the warped image
+            // --use-histogram-matching 0 :: histogram matching is pre-normalisation (we're avoiding this in case we have hyper-intense fat on one and not the other)
+            // --initial-moving-transform [{fixedFile},{floatingFile}, 1] :: inputs
+            // --transform Affine[0.1] :: using an affine transform (12 degrees of freedom, linear)
+            // --metric MI[{fixedFile},{floatingFile},1,32,Regular,0.25] :: metric is mutual information, although we could use cross correlation so long as we're registering the same sequence type
+            // --convergence [1000x500x250x100,1e-6,10] :: convergence is the iterations at each level and the threashold for imporovement in the metric
+            // --shrink-factors 8x4x2x1 :: shrink factors control the resolution at each level
+            // --smoothing-sigmas 3x2x1x0vox :: not really sure what smoothing sigmas do but the values should be fine
+            // --output [_, {niftiOutPath}] :: output the transform value + our sweet nifti file.
+            var args = $@" --dimensionality 3 --float 1 --interpolation Linear --use-histogram-matching 0 --initial-moving-transform [{fixedFile},{floatingFile}, 1] --transform Affine[0.1] --metric MI[{fixedFile},{floatingFile},1,32,Regular,0.25] --convergence [1000x500x250x100,1e-6,10] --shrink-factors 8x4x2x1 --smoothing-sigmas 3x2x1x0vox --output [_, {niftiOutPath}]";
+
+            Tools.ExecProcess("ThirdPartyTools/ANTS/antsRegistration.exe", args, updates);
+
+            return niftiOutPath;
+        }
+
+        public static INifti ANTSRegistration(INifti floating, INifti reference, DataReceivedEventHandler updates = null)
+        {
+            // Setup our temp file names.
+            string niftiInPath = Tools.TEMPDIR + floating.GetHashCode() + ".antsrego.in.nii";
+            string niftiRefPath = Tools.TEMPDIR + floating.GetHashCode() + ".antsrego.ref.nii";
+
+            floating.WriteNifti(niftiInPath);
+            reference.WriteNifti(niftiRefPath);
+
+            string niftiOutPath = ANTSRegistration(niftiInPath, niftiRefPath);
+
+            var output = floating.DeepCopy();
+            output = output.ReadNifti(niftiOutPath);
+
+            return output;
+        }
+
     }
 }
