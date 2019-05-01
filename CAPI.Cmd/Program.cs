a7f0dd1a41748ed95a0e9f54b09706513bcb05c7
plwp <Patrick.Prendergast@mh.org.au>
2019-05-01 11:20:38 +1000

    Trying to add coverage for dicom

--- CAPI.Cmd/Program.cs
+++ CAPI.Cmd/Program.cs
@@ -5,6 +5,7 @@ using System;
 using System.Diagnostics;
 using System.Drawing;
 using System.IO;
+using System.Threading.Tasks;
 
 namespace CAPI.Cmd
 {
@@ -14,10 +15,21 @@ namespace CAPI.Cmd
         {
             Directory.SetCurrentDirectory(AppDomain.CurrentDomain.BaseDirectory + "../../");
 
+            string repo = "D:/Capi-Files/ImageRepository";
+
+            foreach(var arg in args)
+            {
+                if (arg.StartsWith("-r")) repo = arg.Substring(3).Trim(' ');
+            }
+            
+
+            string currentDicomFolder = repo + "/Current/Dicom";
+            string priorDicomFolder = repo + "/Prior/Dicom";
+            string referenceDicomFolder = repo + "/Reference/Dicom";
+
             string floatingPath = "floating.nii";
             string fixedPath = "fixed.nii";
             string outputPrefix = "capiout";
-
 
             if (args.Length >= 2)
             {
@@ -40,8 +52,26 @@ namespace CAPI.Cmd
             totaltime.Start();
 
             IImageProcessor ip = new ImageProcessor(null);
+
+            // Generate Nifti file from Dicom and pass to ProcessNifti Method for current series.
+            System.Console.WriteLine($@"Start converting series dicom files to Nii");
+
+            var currentNifti = ip.DicomToNifti(currentDicomFolder, "current.nii");
+            var priorNifti = ip.DicomToNifti(priorDicomFolder, "prior.nii");
+            var referenceNifti = ip.DicomToNifti(referenceDicomFolder, "reference.nii");
+
+            System.Console.WriteLine($@"Finished converting series dicom files to Nii");
+
             string[] outputs = { outputPrefix + "-increase.nii", outputPrefix + "-decrease.nii" };
-            ip.MSLesionCompare(fixedPath, floatingPath, fixedPath, true, true, true, outputs, "prior-resliced.nii");
+           
+            // Process Nifti files.
+            ip.MSLesionCompare(currentNifti, priorNifti, referenceNifti, true, true, true, outputs, "prior-resliced.nii");
+
+            foreach(var output in outputs)
+            {
+                Console.WriteLine($@"Reading: {output}");
+                new Nifti().ReadNifti(output);
+            }
 
             Console.WriteLine($@" ALL DONE! [{totaltime.Elapsed}]");
             Console.ReadKey();
