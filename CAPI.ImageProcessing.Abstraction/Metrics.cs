cab7b6128b737d5bc9cf465d33c87d9bdd0b98bd
plwp <Patrick.Prendergast@mh.org.au>
2019-05-06 10:07:52 +1000

    Added metrics for MS lesion compare to facilitate metrics slide

---
+++ CAPI.ImageProcessing.Abstraction/Metrics.cs
@@ -0,0 +1,16 @@
+﻿namespace CAPI.ImageProcessing
+{
+    public class Metrics
+    {
+        public double VoxelVolPrior { get; set; }
+        public double VoxelVolCurrent { get; set; }
+        public double VoxelVolUsed { get; set; }
+        public double BrainMatch { get; set; }
+        public double CorrectedBrainMatch { get; set; }
+        public double EdgeRatioIncrease { get; set; }
+        public double EdgeRatioDecrease { get; set; }
+        public bool Passed { get; set; } = true;
+        public string Notes { get; set; }
+
+    }
+}
