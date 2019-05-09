cdee03df4db3eaa84dea1a5de9d3e9489d7f3900
plwp <Patrick.Prendergast@mh.org.au>
2019-05-09 12:28:30 +1000

    Nifti tests, etc.

--- CAPI.Tests/Config/ConfigTests.cs
+++ CAPI.Tests/Config/ConfigTests.cs
@@ -15,12 +15,17 @@ namespace CAPI.Tests.Config
         [TestMethod]
         public void ConfFile()
         {
+            // Delete any prior config files. 
             var confPath = System.IO.Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory, "config.json");
             if (System.IO.File.Exists(confPath)) System.IO.File.Delete(confPath);
 
-            // The first 
-            var conf = CapiConfig.GetConfig();
-            conf = CapiConfig.GetConfig();
+            // The first read will create a new default config file.
+            _ = CapiConfig.GetConfig();
+
+            // The second read will read the created file.
+            _ = CapiConfig.GetConfig();
+
+            // TODO ASSERT some things.
         }
     }
 }
---
+++ CAPI.Tests/NiftiLib/ProcessingTests.cs
@@ -0,0 +1,164 @@
+﻿using CAPI.NiftiLib;
+using CAPI.NiftiLib.Processing;
+using MathNet.Numerics.Statistics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+namespace CAPI.Tests.NiftiLib
+{
+    [TestClass]
+    public class ProcessingTests
+    {
+        string _minimalNiftiPath = $@"{Helper.GetTestResourcesPath()}/nifti/minimal.nii";
+        string _lrNiftiPath = $@"{Helper.GetTestResourcesPath()}/nifti/avg152T1_LR_nifti.nii";
+        string _lrMaskNiftiPath = $@"{Helper.GetTestResourcesPath()}/nifti/avg152T1_LR_nifti_mask.nii";
+
+        [TestMethod]
+        public void BiasCorrectionTest()
+        {
+            if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows)) Assert.Inconclusive("Currently uses Windows Binaries");
+            TestForChanges(BiasCorrection.AntsN4, BiasCorrection.AntsN4, _minimalNiftiPath);
+        }
+
+        [TestMethod]
+        public void BrainExtractionTest()
+        {
+            if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows)) Assert.Inconclusive("Currently uses Windows Binaries");
+            TestForChanges(BrainExtraction.BrainSuiteBSE, BrainExtraction.BrainSuiteBSE, _lrNiftiPath);
+        }
+
+        [TestMethod]
+        public void CompareTest()
+        {
+            // Create som Nifti objects to compare.
+            var niftiA = new Nifti().ReadNifti(_minimalNiftiPath);
+            var niftiB = new Nifti().ReadNifti(_minimalNiftiPath);
+
+            // Mean and standard deviations are used to see if the change is significant, which we want it to be.
+            (var mean, var stdDev) = niftiA.Voxels.Where(val => val > 10).MeanStandardDeviation();
+            var change = (float)stdDev;
+
+            // Make sure the starting values aren't background.
+            niftiA.Voxels[0] = (float)mean;
+            niftiA.Voxels[1] = (float)mean;
+
+            // Increase by 1 std deviation.
+            niftiB.Voxels[0] = niftiA.Voxels[0] + change;
+            // Decrease by 1 std deviation.
+            niftiB.Voxels[1] = niftiA.Voxels[1] - change;
+
+            // Do a heaps compare
+            var decrease = Compare.CompareMSLesionDecrease(niftiB, niftiA);
+            var increase = Compare.CompareMSLesionIncrease(niftiB, niftiA);
+            var changed = Compare.CompareMSLesion(niftiB, niftiA);
+
+            // Check that things are working as expected (within floating point error range)
+            Assert.AreEqual(decrease.Voxels[0], 0, 0.00001, "Problem with CompareMSLesionDecrease");
+            Assert.AreEqual(decrease.Voxels[1], -change, 0.00001, "Problem with CompareMSLesionDecrease"); 
+            Assert.AreEqual(increase.Voxels[0], change, 0.00001, "Problem with CompareMSLesionIncrease");
+            Assert.AreEqual(increase.Voxels[1], 0, 0.00001, "Problem with CompareMSLesionIncrease");
+            Assert.AreEqual(changed.Voxels[1], -change, 0.00001, "Problem with CompareMSLesion");
+            Assert.AreEqual(changed.Voxels[0], change, 0.00001, "Problem with CompareMSLesion");
+
+            // Check that nothing else has changed.
+            var diff = 0f;
+            for (int i = 2; i < niftiA.Voxels.Length; ++i)
+            {
+                diff += Math.Abs(decrease.Voxels[i]);
+                diff += Math.Abs(increase.Voxels[i]);
+                diff += Math.Abs(changed.Voxels[i]);
+            }
+
+            Assert.IsTrue(diff == 0, "We've got extra differences which we shouldn't.");
+        }
+
+        [TestMethod]
+        public void NormalizationTest()
+        {
+            // Setup a couple of niftis. (Reading from files just makes sure we have valid headers, etc.)
+            var niftiA = new Nifti().ReadNifti(_minimalNiftiPath);
+            var niftiB = new Nifti().ReadNifti(_minimalNiftiPath);
+
+            // Lets use a random element in testing! Maybe it'll pass if we get lucky!
+            var rng = new Random(1000);
+            for (int i = 0; i < niftiB.Voxels.Length; ++i)
+            {
+                niftiB.Voxels[i] = rng.Next();
+            }
+
+            // Normalize without trimming any background.
+            niftiB = Normalization.ZNormalize(niftiB, niftiA, float.NegativeInfinity);
+
+            // See how we did.
+            (var mean, var stdDev) = niftiA.Voxels.MeanStandardDeviation();
+            Assert.AreEqual(niftiB.Voxels.Mean(), mean, 0.000001);
+            Assert.AreEqual(niftiB.Voxels.StandardDeviation(), stdDev, 0.000001);
+        }
+
+        [TestMethod]
+        public void RegistrationTest()
+        {
+            if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows)) Assert.Inconclusive("Currently uses Windows Binaries");
+
+            var NiftiA = new Nifti().ReadNifti(_lrMaskNiftiPath);
+            var NiftiB = new Nifti().ReadNifti(_lrNiftiPath);
+
+            // Only doing a couple of asserts here. If something horrible goes wrong, we're expecting an exception.
+            // You can also check the console output to see what the tools reckon.
+            _ = Registration.ANTSRegistration(NiftiA, NiftiB, (d, e) => Console.WriteLine(e.Data));
+            var outFile = Registration.ANTSApplyTransforms(_lrMaskNiftiPath, _lrNiftiPath, (d, e) => Console.WriteLine(e.Data));
+            Assert.IsTrue(File.Exists(outFile), "No out file for ANTSApplyTransforms");
+            _ = new Nifti().ReadNifti(outFile);
+
+            _ = Registration.CMTKRegistration(NiftiA, NiftiB, (d, e) => Console.WriteLine(e.Data));
+            outFile = Registration.CMTKResliceUsingPrevious(_lrMaskNiftiPath, _lrNiftiPath, (d, e) => Console.WriteLine(e.Data));
+            Assert.IsTrue(File.Exists(outFile), "No out file for CMTKResliceUsingPrevious rego");
+            NiftiA.ReadNifti(outFile);
+        }
+
+
+        [TestMethod]
+        public void ToolsTest()
+        {
+            if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows)) Assert.Inconclusive("Currently uses Windows Binaries");
+        }
+
+        private void TestForChanges(Func<string, DataReceivedEventHandler, string> func, Func<INifti, DataReceivedEventHandler, INifti> funcNii, string niftiPath)
+        {
+            // If anything's gone horribly wrong we'll probably get an exception here.
+            var nii = func(niftiPath, (d, e) => Console.WriteLine(e.Data));
+            var nifti = new Nifti().ReadNifti(nii);
+            nifti.RecalcHeaderMinMax();
+
+            // Quick check to see if the file exists and is sane.
+            Assert.IsFalse(nifti.Header.cal_min == nifti.Header.cal_max);
+            // Remove the temp file
+            File.Delete(nii);
+            // Read the input nifti file and run the nifti->nifti version.
+            nifti.ReadNifti(niftiPath);
+            var outnifti = funcNii(nifti, (d, e) => Console.WriteLine(e.Data));
+            // Check that there has been some change made to the voxels.
+            var diff = 0f;
+            for (int i = 0; i < outnifti.Voxels.Length; ++i)
+            {
+                diff += Math.Abs(nifti.Voxels[i] - outnifti.Voxels[i]);
+            }
+
+            // Some change has happened.
+            Assert.IsFalse(diff == 0);
+        }
+
+        [TestCleanup]
+        public void TestCleanUp()
+        {
+            foreach (var f in Directory.GetFiles("./", "temp*.nii"))
+            {
+                File.Delete(f);
+            }
+        }
+    }
+}
