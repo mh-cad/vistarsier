600daf7b0e3f4ab3a5adcd398fe52aa4ae2fdb53
plwp <Patrick.Prendergast@mh.org.au>
2019-05-09 12:28:30 +1000

    Removed UAT, since what it's testing is now way out of date and is better done in unit tests.

---
+++ CAPI.Tests/Service/DBConnection.cs
@@ -0,0 +1,26 @@
+﻿using CAPI.Service.Agent;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CAPI.Tests.Service
+{
+    [TestClass]
+    public class DBConnection
+    {
+        [TestMethod]
+        public void TestConnection()
+        {
+            var connectionString = CapiConfigGetter.GetCapiConfig().AgentDbConnectionString;
+
+            try
+            {
+                var broker = new DbBroker(connectionString);
+
+                var dbExists = broker.Database.EnsureCreated();
+
+                Assert.IsTrue(dbExists);
+            }
+            catch
+            { }
+        }
+    }
+}
