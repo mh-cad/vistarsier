10cbb23d64ce422c85f058162bf96c749ee3f86a
plwp <Patrick.Prendergast@mh.org.au>
2019-06-04 14:23:26 +1000

    Feeling generalised. Might delete l8r, idk.

---
+++ VisTarsier.MS/IClassifier.cs
@@ -0,0 +1,14 @@
+﻿using VisTarsier.NiftiLib;
+
+namespace VisTarsier.MS
+{
+    interface IClassifier<T>
+    {
+        /// <summary>
+        /// The classifier will produce a mask/labelset based on one or more inputs.
+        /// </summary>
+        /// <param name="inputs">Input niftis</param>
+        /// <returns>The output nifti</returns>
+        INifti<T> Classify(INifti<T>[] inputs);
+    }
+}
