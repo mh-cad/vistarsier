8f8703113f664275b1b395c3ec6bd13324efa64a
plwp <Patrick.Prendergast@mh.org.au>
2019-06-04 14:23:26 +1000

    Feeling generalised. Might delete l8r, idk.

---
+++ VisTarsier.MS/IncreaseClassifier.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using VisTarsier.NiftiLib;
+using VisTarsier.NiftiLib.Processing;
+
+namespace VisTarsier.MS
+{
+    class CapiClassifier : IClassifier<float>
+    {
+        public INifti<float> Classify(INifti<float> prior, INifti<float> current)
+        {
+            var output = Compare.CompareMSLesionIncrease(prior, current);
+            return output;
+        }
+
+        public INifti<float> Classify(INifti<float>[] inputs)
+        {
+            if (inputs.Length != 2) throw new ArgumentException("This classifier needs a prior and current INifti");
+
+            var prior = inputs[0];
+            var current = inputs[1];
+
+            return Classify(prior, current);
+        }
+    }
+}
