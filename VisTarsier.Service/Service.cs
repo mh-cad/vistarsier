1a23c98ad552c704276d6a6240b3d11cd9f9bc4b
plwp <Patrick.Prendergast@mh.org.au>
2020-02-10 14:10:50 +1100

    Extra checks for DB creation and connection (issue #8)

--- VisTarsier.Service/Service.cs
+++ VisTarsier.Service/Service.cs
@@ -74,33 +74,69 @@ namespace VisTarsier.Service
                 || !File.Exists(conf.Binaries.dcm2niix)
                 || !File.Exists(conf.Binaries.bse))
             {
-                throw new FileNotFoundException("Could not find one or more essential binaries as referenced in config.json");
+                var badFiles = new List<string>();
+                if (!File.Exists(conf.Binaries.antsRegistration)) badFiles.Add(conf.Binaries.antsRegistration);
+                if (!File.Exists(conf.Binaries.antsApplyTransforms)) badFiles.Add(conf.Binaries.antsApplyTransforms);
+                if (!File.Exists(conf.Binaries.N4BiasFieldCorrection)) badFiles.Add(conf.Binaries.N4BiasFieldCorrection);
+                if (!File.Exists(conf.Binaries.img2dcm)) badFiles.Add(conf.Binaries.img2dcm);
+                if (!File.Exists(conf.Binaries.dcm2niix)) badFiles.Add(conf.Binaries.dcm2niix);
+                if (!File.Exists(conf.Binaries.dcm2niix)) badFiles.Add(conf.Binaries.bse);
+                
+                throw new FileNotFoundException(String.Join("Could not find one or more essential binaries as referenced in config.json. ", badFiles.ToArray()));
             }
         }
 
         private void CleanupDatabase()
         {
+            var log = Log.GetLogger();
             var cfg = CapiConfig.GetConfig();
             if (cfg == null) throw new ApplicationException("Unable to find config file.");
             var dbBroker = new DbBroker(cfg.AgentDbConnectionString);
-            dbBroker.Database.EnsureCreated();
-            var failedCases = dbBroker.GetCaseByStatus("Processing");
+            
+            if(dbBroker.Database.EnsureCreated())
+            {
+                log.Info("Database not found, so it has been created.");
+            }
+            else
+            {
+                log.Info("Database found.");
+            }
 
-            foreach (var c in failedCases)
+            if(!dbBroker.Database.CanConnect())
             {
-                var tmp = c;
-                tmp.Status = "Pending";
-                dbBroker.Attempts.Update(tmp);
-                dbBroker.SaveChanges();
+                log.Error("Could not connect to database using string: " + cfg.AgentDbConnectionString);
             }
-            var failedJobs = dbBroker.GetJobByStatus("Processing");
-            foreach (var j in failedJobs)
+            else
             {
-                var tmp = j;
-                tmp.Status = "Failed";
-                dbBroker.Jobs.Update(tmp);
-                dbBroker.SaveChanges();
+                log.Info("DB Connection established");
             }
+
+            try
+            {
+                var failedCases = dbBroker.GetCaseByStatus("Processing");
+
+                foreach (var c in failedCases)
+                {
+                    var tmp = c;
+                    tmp.Status = "Pending";
+                    dbBroker.Attempts.Update(tmp);
+                    dbBroker.SaveChanges();
+                }
+                var failedJobs = dbBroker.GetJobByStatus("Processing");
+                foreach (var j in failedJobs)
+                {
+                    var tmp = j;
+                    tmp.Status = "Failed";
+                    dbBroker.Jobs.Update(tmp);
+                    dbBroker.SaveChanges();
+                }
+            }
+            catch (Exception e)
+            {
+                log.Error("Error while accesing database. If this is due to an incompatible schema, maybe try wiping the database and starting again?");
+                log.Error(e);
+            }
+            
         }
 
         protected override void OnStop()
@@ -108,3 +144,4 @@ namespace VisTarsier.Service
         }
     }
 }
+ 
