14ea3995ce6834d6149655c2c82ce80f17a5de99
plwp <Patrick.Prendergast@mh.org.au>
2020-02-10 16:10:36 +1100

    Installer tweaks

--- VisTarsier.ConfigApp/MainWindow.xaml.cs
+++ VisTarsier.ConfigApp/MainWindow.xaml.cs
@@ -27,10 +27,25 @@ namespace VisTarsier.ConfigApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        // Prep stuff needed to remove close button on window
+        private const int GWL_STYLE = -16;
+        private const int WS_SYSMENU = 0x80000;
+        [System.Runtime.InteropServices.DllImport("user32.dll", SetLastError = true)]
+        private static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        private static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+
         //private static string[] _aeDestVals;
+        void ToolWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Code to remove close box from window
+            var hwnd = new System.Windows.Interop.WindowInteropHelper(this).Handle;
+            SetWindowLong(hwnd, GWL_STYLE, GetWindowLong(hwnd, GWL_STYLE) & ~WS_SYSMENU);
+        }
 
         public MainWindow()
         {
+            Loaded += ToolWindow_Loaded;
             InitializeComponent();
             chkSameDest.Checked += ChkSameDest_Checked;
             chkSameDest.Unchecked += ChkSameDest_Unchecked;
